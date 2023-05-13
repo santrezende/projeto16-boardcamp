40a78b3cb384d1e0a8f7d5fbcd11400be1f67213
santiago <santiagorsilva99@gmail.com>
2023-05-13 02:59:49 -0300

    fix: change main content on package.json 2

--- src/app.js
+++ src/app.js
@@ -8,5 +8,5 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
-const PORT = 4000
+const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
